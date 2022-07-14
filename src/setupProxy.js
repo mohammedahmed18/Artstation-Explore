3d46534c9027975783b076b7b6b846d77586629f
mohammed ahmed <mohammed18200118@gmail.com>
2022-07-14 15:38:10 +0200

    fix

--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const proxy = {
-  target: "https://www.artstation.com",
-  changeOrigin: true,
-};
-module.exports = function (app) {
-  app.use("/**", createProxyMiddleware(proxy));
-};
