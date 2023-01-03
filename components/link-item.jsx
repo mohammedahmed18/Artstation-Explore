64765d2bd5bf5b3e0a28f5e71c7149ba9d4f8674
mohammedahmed18 <mohammed18200118@gmail.com>
2023-01-03 14:41:30 +0200

    fix icon sizes

--- components/link-item.jsx
+++ components/link-item.jsx
@@ -4,7 +4,7 @@ const LinkItem = ({ text, icon, link }) => {
   return (
     // <li className="flex items-center">
       <a target="_blank" rel="noreferrer" href={link} className="btn btn-ghost">
-        <FontAwesomeIcon icon={icon} size="2x" />
+        <FontAwesomeIcon icon={icon} size="2x" width={25} height={25}/>
         <span className="ml-2 text-primary-content">{text}</span>
       </a>
     // </li>
